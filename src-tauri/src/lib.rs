8a68ff509ca714829b1f1d4b3e269e81f418650f
paiyayay <paixeww@gmail.com>
2024-08-24 15:36:20 +0300

    removed fs plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -101,7 +101,6 @@ fn get_music(_dir: &str) -> Vec<PathBuf> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             get_music,
